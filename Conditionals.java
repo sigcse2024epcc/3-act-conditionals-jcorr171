06441f251042b2edae9f124d5281c87f91f526c8
jcorr171 <jcorr171@my.epcc.edu>
2026-07-07 10:55:09 -0600

    Add input handling and comparison logic for numbers
    Implemented user input for two integers and compared them using if/else statements.

--- Conditionals.java
+++ Conditionals.java
@@ -1,27 +1,25 @@
-// TODO 1: Import the Scanner library here!
-
+import java.util.Scanner;
 
 public class Conditionals {
-    
     public static void main(String[] args) {
-        
-        // TODO 2: Initialize the Scanner to read from System.in
-        
-        
-        // TODO 3: Prompt the user with "Number: " and read the first integer
-        
-        
-        // TODO 4: Prompt the user with "Number: " and read the second integer
-        
-        
-        // TODO 5: Create an if/else statement to compare the two numbers.
-        // If the first number is smaller, print it first.
-        // Else, print the second number first.
-        // ADVERSARIAL HINT: Don't forget the comma and space between the numbers!
-        
-        
-        
-        // Good practice: close your Scanner when you are done!
-        // input.close();
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Number: ");
+        int num1 = input.nextInt();
+
+        System.out.print("Number: ");
+        int num2 = input.nextInt();
+
+        if (num1 < num2) {
+            System.out.println(num1 + ", " + num2);
+        }
+
+        else if (num2 < num1){
+            System.out.println(num2 + ", " + num1);
+        }
+        else if (num1 == num2) {
+            System.out.println("They are the same number : " + num1 + " , " + num2);
+        }
     }
 }
